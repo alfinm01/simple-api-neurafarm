c7e9ae7151eed56603bd0bc98c8bde3a364b138c
Alfian Maulana <alfianm.ibrahim@gmail.com>
2020-03-24 12:24:14 +0700

    Create endpoints skeleton

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* Check if API is live. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('You are connected');
 });
+
+/* Login endpoint. */
+router.post('/login', (req, res) => {
+  res.send('login');
+});
+
+/* Upload file endpoint. */
+router.post('/upload-file', (req, res) => {
+  res.send('upload-file');
+});
+
+router.post('/recover', (req, res) => {
+	// Sending email to user containing link
+	// Link is used to change password but with Postman
+	// Link is in form of hashed email
+	res.send('Use this link to change your password via Postman or similar application. [POST] /change-password/:id')
+})
+
+router.post('/change-password/:id', (req, res) => {
+	res.send('Success')
+})
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+/* GET all users. */
+router.get('/', (req, res) => {
+  res.send('respond with a resource');
+});
+
+/* GET user by id. */
+router.get('/:id', (req, res) => {
+  res.send('respond with a resource');
+});
+
+/* POST new user. */
+router.post('/', (req, res) => {
+  res.send('respond with a resource');
+});
+
+/* UPDATE user info. */
+router.put('/:id', (req, res) => {
   res.send('respond with a resource');
 });
 
