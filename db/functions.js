5c668b16570c3d57962bdca59cd123063fae4d79
Alfian Maulana <alfianm.ibrahim@gmail.com>
2020-03-24 14:57:48 +0700

    Fix db connection bug

--- db/functions.js
+++ db/functions.js
@@ -2,28 +2,26 @@ var client = require('./db_config')
 
 // await client.connect itu masih belum best practice?
 
-// error if ran after function2
 const function1 = async () => {
   await client.connect()
   const collection = await client.db('test').collection('devices')
   const results = await collection.find().toArray()
-  await client.close()
+  // await collection.find().close()
   return results
 }
 
-// error if ran after function1
 const function2 = async () => {
   await client.connect()
   const collection = await client.db('test').collection('devices')
-  await collection.insertMany([
+  const results = await collection.insertMany([
     { item: 'journal', qty: 25, status: 'A', size: { h: 14, w: 21, uom: 'cm' }, tags: ['blank', 'red'] },
     { item: 'notebook', qty: 50, status: 'A', size: { h: 8.5, w: 11, uom: 'in' }, tags: ['red', 'blank'] },
     { item: 'paper', qty: 10, status: 'D', size: { h: 8.5, w: 11, uom: 'in' }, tags: ['red', 'blank', 'plain'] },
     { item: 'planner', qty: 0, status: 'D', size: { h: 22.85, w: 30, uom: 'cm' }, tags: ['blank', 'red'] },
     { item: 'postcard', qty: 45, status: 'A', size: { h: 10, w: 15.25, uom: 'cm' }, tags: ['blue'] }
   ])
-  await client.close()
-  return 'success?'
+  // await collection.insertMany().close()
+  return results
 }
 
 module.exports = {
